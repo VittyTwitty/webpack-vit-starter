f6c731d9fb76e963a07260cc986a96cdf33b933e
VittyTwitty <badvet93@gmail.com>
2017-09-13 09:18:28 +0300

    version 1.0.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require('path');
 const webpack = require('webpack');
+const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
